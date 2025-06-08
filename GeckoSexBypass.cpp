ec11b45334459670d965ac52b2817fe123c37e7a
Xreped <162215420+Xreped@users.noreply.github.com>
2025-06-08 23:21:31 +0400

    code upd

---
+++ GeckoSexBypass.cpp
@@ -0,0 +1,16 @@
+﻿#include <windows.h>
+#include <winternl.h>
+#include <iostream>
+
+#pragma comment(lib, "cecko.lib")
+int main() {
+    HMODULE sex = LoadLibraryA("cecko.dll");
+
+    if (!sex) {
+        std::cerr << "ERROR cecko.dll" << std::endl;
+        return 1;
+    }
+
+    CeckoBrainSet("genderSex", 0)
+    return 0;
+}
